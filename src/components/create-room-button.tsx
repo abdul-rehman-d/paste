0ba3616dd23155d3087bb686bdaa7834102f2010
Abdul Rehman <ar618211@gmail.com>
2025-12-21 20:22:06 +0500

    create dialog and its component - just poc yet

--- src/components/create-room-button.tsx
+++ src/components/create-room-button.tsx
@@ -1,5 +1,47 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
 export function CreateARoomButton() {
-	return <Button>Create a Room</Button>;
+  return (
+    <Dialog>
+      <DialogTrigger className={buttonVariants({ variant: "default" })}>
+        Create a Room
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create a Room</DialogTitle>
+          <InputOTPDemo />
+        </DialogHeader>
+        <DialogFooter className="border-t py-2">
+          <Button>Create</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function InputOTPDemo() {
+  return (
+    <InputOTP maxLength={4}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+        <InputOTPSlot index={2} />
+        <InputOTPSlot index={3} />
+      </InputOTPGroup>
+    </InputOTP>
+  );
 }
