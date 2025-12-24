6a97365f56cab149f6959192e35221edb890d112
Abdul Rehman <ar618211@gmail.com>
2025-12-25 04:16:53 +0500

    feat: enhance item listing with copy functionality and loading state

--- src/app/(with-main-layout)/[roomSlug]/unlocked/client-page.tsx
+++ src/app/(with-main-layout)/[roomSlug]/unlocked/client-page.tsx
@@ -1,14 +1,55 @@
 "use client";
 
 import { useQuery } from "convex/react";
+import { useCallback } from "react";
+import { toast } from "sonner";
+import { lockRoom } from "@/app/actions";
+import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
+import type { Doc } from "@/convex/_generated/dataModel";
 
 export default function RoomPageClient({ roomSlug }: { roomSlug: string }) {
+  return (
+    <>
+      <div className="flex justify-end">
+        {/* <form action={lockRoom}>
+          <Button type="submit">Lock Room</Button>
+        </form> */}
+      </div>
+
+      <div className="space-y-4">
+        <ItemsList roomSlug={roomSlug} />
+      </div>
+    </>
+  );
+}
+
+function ItemsList({ roomSlug }: { roomSlug: string }) {
   const items = useQuery(api.item.listByRoom, { roomSlug: roomSlug });
 
-  if (!items || !items.length) {
+  if (!items) return <p>loading...</p>;
+
+  if (!items.length) {
     return <p>no items</p>;
   }
 
-  return items.map((item) => <li key={item._id}>{item.text}</li>);
+  return items.map((item) => <Item key={item._id} item={item} />);
+}
+
+function Item({ item }: { item: Doc<"items"> }) {
+  const copy = useCallback(() => {
+    navigator.clipboard.writeText(item.text);
+    toast("copied!");
+  }, [item]);
+  return (
+    <div className="">
+      <Button
+        className="flex border rounded-xs p-4 w-full"
+        variant="boring"
+        onClick={copy}
+      >
+        <p>{item.text}</p>
+      </Button>
+    </div>
+  );
 }
